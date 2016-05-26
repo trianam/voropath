d96962dec841f599efee64f48a43594391c165e4
Stefano Martina <stefano.martina@gmail.com>
2016-05-26 17:52:36 +0200

    Added plot scene choices.

--- plotScene.py
+++ plotScene.py
@@ -5,6 +5,15 @@ import pickle
 import plotter
 
 if len(sys.argv) >= 2:
+    if len(sys.argv) == 4:
+        i = 2
+        plotSites = bool(eval(sys.argv[i]))
+        i += 1
+        plotGraph = bool(eval(sys.argv[i]))
+    else:
+        plotSites = bool(eval(input('Do you want to plot Voronoi sites? (True/False): ')))
+        plotGraph = bool(eval(input('Do you want to plot graph? (True/False): ')))
+
 
     print('Load file', flush=True)
     with open(sys.argv[1], 'rb') as f:
@@ -16,11 +25,13 @@ if len(sys.argv) >= 2:
     plt = plotter.Plotter()
     
     voronoi.plotPolyhedrons(plt, verbose = True)
-    voronoi.plotSites(plt, verbose = True)
-    voronoi.plotGraph(plt, verbose = True)
+    if plotSites:
+        voronoi.plotSites(plt, verbose = True)
+    if plotGraph:
+        voronoi.plotGraph(plt, verbose = True)
 
     print('Render', flush=True)
     plt.draw()
     
 else:
-    print('use: {} sceneFile'.format(sys.argv[0]))
+    print('use: {} sceneFile [plotSites plotGraph]'.format(sys.argv[0]))
